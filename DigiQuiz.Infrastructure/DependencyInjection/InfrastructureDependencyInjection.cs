d7c40147af239f802b283f13923f95cb94bf20e5
Kiomars93 <kiomars.mirsaeedi@hotmail.com>
2023-11-07 18:57:57 +0100

    Registering the PlayerRepository, adding migration with EFCore.

--- DigiQuiz.Infrastructure/DependencyInjection/InfrastructureDependencyInjection.cs
+++ DigiQuiz.Infrastructure/DependencyInjection/InfrastructureDependencyInjection.cs
@@ -1,5 +1,8 @@
 ﻿using DigiQuiz.Application.Interfaces;
+using DigiQuiz.Infrastructure.Data.DbContexts;
+using DigiQuiz.Infrastructure.Repositories;
 using DigiQuiz.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,10 +10,13 @@ namespace DigiQuiz.Infrastructure.DependencyInjection;
 
 public static class InfrastructureDependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddTransient<IDigimonRepository, DigimonRepository>();
+        services.AddTransient<IDigimonRepository, DigimonRepository>()
+            .AddScoped<IPlayerRepository, PlayerRepository>();
         services.AddHttpClient();
+        var connectionstring = configuration.GetConnectionString("DatabaseConnection");
+        services.AddDbContext<PlayerDbContext>(opt => opt.UseSqlServer(connectionstring));
         return services;
     }
 }
---
+++ DigiQuiz.Infrastructure/Migrations/20231107135312_Init.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DigiQuiz.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Points = table.Column<int>(type: "int", nullable: false),
+                    GameDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Player");
+        }
+    }
+}
