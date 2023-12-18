08f170a6d3ee49815f185e3ce1bc2cbd05d8dc10
Kiomars93 <kiomars.mirsaeedi@hotmail.com>
2023-12-18 16:49:45 +0100

    Receiving the backend data correctly. Having issue with state handling to totalpoints atm before the navigation. Changing from alert to a pop-up module.

--- frontend/DigiQuiz.UI/src/components/Quiz.tsx
+++ frontend/DigiQuiz.UI/src/components/Quiz.tsx
@@ -12,7 +12,8 @@ export default function Quiz({
   updateTotalPointsState,
 }: TotalPointProps) {
   const navigate = useNavigate();
-  const [digimons, setDigimons] = useState<Digimons[] | null>([]);
+  const [digimonsData, setDigimonsData] = useState<Digimons[] | null>([]);
+  const [digimonsQuestion, setDigimonsQuestion] = useState<Digimons[]>([]);
   const [correctAnswerId, setCorrectAnswerId] = useState<number | null>(null);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [currentImage, setCurrentImage] = useState<string>('');
@@ -29,7 +30,9 @@ export default function Quiz({
     const fetchDataAsync = async (url: string) => {
       try {
         const result: Digimons[] = await fetchData(url);
-        setDigimons(result);
+        setDigimonsData(result);
+        updateTotalPointsState(0);
+        setCurrentPage(1);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -38,38 +41,63 @@ export default function Quiz({
     fetchDataAsync(`${baseUrl}/Questions`);
   }, []);
 
-  if (digimons === null || digimons === undefined) {
+  //Todo: null check:a bättre
+  if (digimonsData === null || digimonsData === undefined) {
+    console.log('brb');
     return <p>Loading...</p>;
   }
 
-  const initRandomIndex = Math.floor(Math.random() * digimons.length);
   useEffect(() => {
-    if (digimons.length > 0) {
-      setCurrentImage(digimons[initRandomIndex].image || '');
-      setCorrectAnswerId(digimons[initRandomIndex].id);
+    if (digimonsData.length > 0 && digimonsQuestion.length < 3) {
+      updateQuestionRound();
     }
-  }, [digimons]);
+  }, [digimonsData, digimonsQuestion]);
 
-  const handleAnswersClick = (digimonId: number) => {
-    if (currentPage === 10) {
-      alert('End game');
-      navigate('/scoreboard');
-    }
+  const updateQuestionRound = () => {
+    const remainingQuestions = digimonsData.filter(
+      (digimon) =>
+        !digimonsQuestion.some((selectedItem) => selectedItem.id === digimon.id)
+    );
 
-    if (digimonId === correctAnswerId) {
-      const randomIndex = Math.floor(Math.random() * digimons.length);
-      console.log('Correct!');
-      updateTotalPointsState(totalPointsprops + 5);
-      setCurrentImage(digimons[randomIndex].image);
-      setCorrectAnswerId(digimons[randomIndex].id);
-    } else {
-      console.log('Wrong!');
-    }
-    setCurrentPage(currentPage + 1);
-    console.log(currentPage);
+    const randomThreeItems = getRandomThreeItems(remainingQuestions);
+    setDigimonsQuestion([...randomThreeItems]);
   };
 
-  console.log(digimons);
+  const getRandomThreeItems = (items: Digimons[]): Digimons[] => {
+    const shuffledItems = [...items].sort(() => Math.random() - 0.5);
+    return shuffledItems.slice(0, 3);
+  };
+
+  const initRandomIndex = Math.floor(Math.random() * digimonsQuestion.length);
+
+  useEffect(() => {
+    if (digimonsQuestion.length > 0) {
+      setCurrentImage(digimonsQuestion[initRandomIndex].image || '');
+      setCorrectAnswerId(digimonsQuestion[initRandomIndex].id);
+    }
+  }, [digimonsQuestion]);
+
+  const handleAnswersClick = (digimonId: number) => {
+    const randomIndex = Math.floor(Math.random() * digimonsQuestion.length);
+    setCurrentImage(digimonsQuestion[randomIndex].image);
+    setCorrectAnswerId(digimonsQuestion[randomIndex].id);
+
+    if (digimonId === correctAnswerId) {
+      updateTotalPointsState(totalPointsprops + 5);
+    }
+
+    setCurrentPage((prevPage) => maxPageState(prevPage + 1));
+
+    if (currentPage === 10) {
+      navigate('/scoreboard');
+    } else {
+      updateQuestionRound();
+    }
+  };
+
+  const maxPageState = (newState: number) => {
+    return Math.min(newState, 10);
+  };
 
   return (
     <>
@@ -77,9 +105,15 @@ export default function Quiz({
       <h2>{currentPage}/10</h2>
       <h1>What is the name of this digimon?</h1>
 
-      <img src={currentImage} alt={`Image not found`} />
+      <img
+        height='300'
+        width='300'
+        src={currentImage}
+        alt={`Image not found`}
+        style={{ borderRadius: '5%', border: 'solid' }}
+      />
       <ul>
-        {digimons.map((digimon) => (
+        {digimonsQuestion.map((digimon) => (
           <li style={{ listStyle: 'none' }} key={digimon.id}>
             <button
               onClick={() => handleAnswersClick(digimon.id)}
