de64e8b013241dfe2d90794332d670141c2af593
Kiomars93 <kiomars.mirsaeedi@hotmail.com>
2023-12-19 09:49:24 +0100

    Creating SSL for dev env to run https

--- frontend/DigiQuiz.UI/vite.config.ts
+++ frontend/DigiQuiz.UI/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      // dev specific config
+      plugins: [react(), mkcert()],
+    };
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+    };
+  }
+});
